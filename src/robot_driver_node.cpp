650b3f0258ec207e81e98fa3d11d7e1d13dce829
Griffin Tabor <gftabor@wpi.edu>
2017-10-17 16:01:55 -0400

    Reads UART waiting for start byte. Time out of 5 ms.

--- src/robot_driver_node.cpp
+++ src/robot_driver_node.cpp
@@ -1,35 +1,98 @@
 #include <ros/ros.h>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 
 #include <iostream>
+
+//Start byte 
+//subsystem ID
+//data 4 bytes
+//checksum
+
+
+
+void read_callback(bool& data_available, boost::asio::deadline_timer& timeout, const boost::system::error_code& error, std::size_t bytes_transferred)
+{
+  if (error || !bytes_transferred)
+  {
+    // No data was read!
+    data_available = false;
+    return;
+  }
+
+  timeout.cancel();  // will cause wait_callback to fire with an error
+  data_available = true;
+}
+
+void wait_callback(boost::asio::serial_port& ser_port, const boost::system::error_code& error)
+{
+  if (error)
+  {
+    // Data was read and this timeout was canceled
+    return;
+  }
+
+  ser_port.cancel();  // will cause read_callback to fire with an error
+}
 
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "robot_publisher");
+  ros::init(argc, argv, "robot_driver_node");
   ros::NodeHandle n;
   std::string port;
   int baud_rate;
   std::string frame_id;
 
-  n.getParam("/robot_driver/port", port);
-  n.getParam("/robot_driver/baud_rate", baud_rate);
-  n.getParam("/robot_driver/frame_id", frame_id);
+  n.getParam("/robot_driver_node/port", port);
+  n.getParam("/robot_driver_node/baud_rate", baud_rate);
+  n.getParam("/robot_driver_node/frame_id", frame_id);
   ROS_INFO("Running with port: %s and baud rate: %d", port.c_str(), baud_rate);
 
   boost::asio::io_service io;
+  boost::asio::deadline_timer timeout(io);
 
+  unsigned char  my_buffer[1];
+  bool data_available = false;
+  boost::array<uint8_t, 5> out;
+
+  out[0]='a';
+  out[1]='b';
+  out[2]='c';
+  out[3]='d';
+  out[4]='\n';
 
   try
   {
     boost::asio::serial_port serial_(io,port); // UART port for the Cortex
     serial_.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
 
+    int runs =0;
+    ROS_INFO("about to recieve");
+
     while (ros::ok())
     {
+      runs ++;
      //ros::Time::now();
-	   
+      serial_.async_read_some(boost::asio::buffer(my_buffer),
+        boost::bind(&read_callback, boost::ref(data_available), boost::ref(timeout),
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+      timeout.expires_from_now(boost::posix_time::milliseconds(5)); //500 ms timeout
+      timeout.async_wait(boost::bind(&wait_callback, boost::ref(serial_),
+        boost::asio::placeholders::error));
+
+      io.run();
+      io.reset();
+      //boost::asio::write(serial_,  boost::asio::buffer(&out[0], 5));
+
+      ROS_INFO("recieved %d",data_available);
+      data_available = 0;
+
       ros::spinOnce();
     }
 
