8dafe351dd53f580c3b68ebf6b4cfa3a86660184
Jonathan Berry <jberry@wpi.edu>
2017-10-22 23:47:39 -0400

    finshed vel and acc plotting

--- src/vel_log_node.py
+++ src/vel_log_node.py
@@ -2,15 +2,19 @@
 
 ## Simple velocity logger that listens to std_msgs/Int16
 ##   published to the 'lwheel' and 'rwheel' topics.
-## After 1000 data points from both, produces a graph of
-##   the data.
+## After five seconds of not receiving data from either
+##   topic, stops collection and produces graphs of:
+##     ticks over time
+##     m/s over time
+##     velocity over time
+##     acceleration over time
 
 import rospy
 from std_msgs.msg import Int16
 import numpy
 import matplotlib.pyplot as plot
 
-last_recieved_data = 0
+last_recieved_data = 0 # the rospy Time when data was last received
 
 lwheel = []
 rwheel = []
@@ -20,19 +24,25 @@ def left_callback(data):
     last_recieved_data = rospy.Time.now()
     rospy.loginfo(rospy.get_caller_id() + 'right: %s', data.data)
     lwheel.append(data.data)
-    #numpy.append(lwheel, 0)#[rospy.Time.now().to_sec(), data.data], 0)
 
 def right_callback(data):
     global rwheel
     last_recieved_data = rospy.Time.now()
     rospy.loginfo(rospy.get_caller_id() + 'left: %s', data.data)
-    numpy.append(rwheel, 0)#[rospy.Time.now().to_sec(), data.data], 0)
     rwheel.append(data.data)
 
-def meters_per_second(ticks):
+# from a list of ticks 15ms apart, returns a list of "instantaneous" velocities
+def velocities(ticks):
     ticks_in_meter = 1390
-    meters_traveled = [tick/ticks_in_meter for tick in ticks]
-    
+    ms_between_ticks = 15
+    diff_ticks = numpy.diff(ticks)
+    meters_traveled = [diff_ticks/ticks_in_meter for tick in diff_ticks]
+    return [((meters_traveled*1000)/ms_between_ticks) for meters_traveled in meters_traveled]
+
+# from a list of velocity values 15ms apart, returns a list of "instantaneous" accelerations
+def accelerations(velocities):
+    ms_between_values = 15
+    return [(velocity*1000)/ms_between_values for velocity in velocities]
 
 def listener():
     global lwheel
@@ -43,39 +53,29 @@ def listener():
 
     last_recieved_data = rospy.Time.now()
 
-    # collect data until it has been more than a minute
-    # since the last data was recieved, exit this loop
+    # collect data until it has been more than five
+    #   seconds since the last data was received
     while rospy.Time.now() - last_recieved_data < rospy.Duration.from_sec(5):
          rospy.sleep(rospy.Duration(1, 0))
-
-    #rospy.loginfo(rospy.get_caller_id() + 'left: %s', numpy.array_str(lwheel))
-    #rospy.loginfo(rospy.get_caller_id() + 'right: %s', numpy.array_str(rwheel))
-
-    #print(lwheel)
-    #print(rwheel)
 
     # plot raw encoder ticks over time
     plot.plot(lwheel)
     plot.plot(rwheel)
     plot.show()
 
-    # plot m/s over time
-    plot.plot(meters_per_second(lwheel))
-    plot.plot(meters_per_second(rwheel))
+    # plot velocity between intervals over time
+    lvels = velocities(lwheel)
+    rvels = velocities(rwheel)
+    plot.plot(lvels)
+    plot.plot(rvels)
     plot.show()
-    
-    # plot acceleration over time
-    ldelta = numpy.diff(lwheel)
-    rdelta = numpy.diff(rwheel)
-    lvel = [(delta*1000)/15 for delta in ldelta]
-    rvel = [(delta*1000)/15 for delta in rdelta]
-    plot.plot(lvel)
-    plot.plot(rvel)
+
+    # plot acceleration between intervals over time
+    lacc = accelerations(lvels)
+    racc = accelerations(rvels)
+    plot.plot(lacc)
+    plot.plot(racc)
     plot.show()
-    
-    # plot velocity over time
-    
 
 if __name__ == '__main__':
     listener()
-
