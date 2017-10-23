047449bc64d2b1e7cf825dd96b7146b8cc8b0a88
Jetson <gftabor@wpi.edu>
2017-10-22 21:31:08 -0400

    fixed plotting, halfway though adding more plots

--- src/vel_log_node.py
+++ src/vel_log_node.py
@@ -10,37 +10,71 @@ from std_msgs.msg import Int16
 import numpy
 import matplotlib.pyplot as plot
 
-last_recieved_data = rospy.Time.now()
+last_recieved_data = 0
 
 lwheel = []
 rwheel = []
 
 def left_callback(data):
+    global lwheel
     last_recieved_data = rospy.Time.now()
     rospy.loginfo(rospy.get_caller_id() + 'right: %s', data.data)
-    numpy.append(lwheel, [rospy.Time.now().to_sec(), data.data], 2)
+    lwheel.append(data.data)
+    #numpy.append(lwheel, 0)#[rospy.Time.now().to_sec(), data.data], 0)
 
 def right_callback(data):
+    global rwheel
     last_recieved_data = rospy.Time.now()
     rospy.loginfo(rospy.get_caller_id() + 'left: %s', data.data)
-    time = rospy.Time.now()
-    time = time.to_sec()
-    numpy.append(rwheel, [rospy.Time.now().to_sec(), data.data], 2)
+    numpy.append(rwheel, 0)#[rospy.Time.now().to_sec(), data.data], 0)
+    rwheel.append(data.data)
+
+def meters_per_second(ticks):
+    ticks_in_meter = 1390
+    meters_traveled = [tick/ticks_in_meter for tick in ticks]
+    
 
 def listener():
+    global lwheel
+    global rwheel
     rospy.init_node('vel_log', anonymous=True)
     rospy.Subscriber('lwheel', Int16, left_callback)
     rospy.Subscriber('rwheel', Int16, right_callback)
-    
-    # collect data until it has been more than a minute 
+
+    last_recieved_data = rospy.Time.now()
+
+    # collect data until it has been more than a minute
     # since the last data was recieved, exit this loop
-    while rospy.Time.now() - last_recieved_data > rospy.Duration.fromsec(60):
-        rospy.spinOnce()
-    
+    while rospy.Time.now() - last_recieved_data < rospy.Duration.from_sec(5):
+         rospy.sleep(rospy.Duration(1, 0))
+
+    #rospy.loginfo(rospy.get_caller_id() + 'left: %s', numpy.array_str(lwheel))
+    #rospy.loginfo(rospy.get_caller_id() + 'right: %s', numpy.array_str(rwheel))
+
+    #print(lwheel)
+    #print(rwheel)
+
     # plot raw encoder ticks over time
-    plot.matshow(lwheel, cmap='Blues')
-    plot.matshow(rwheel, cmap='Greens')
+    plot.plot(lwheel)
+    plot.plot(rwheel)
     plot.show()
+
+    # plot m/s over time
+    plot.plot(meters_per_second(lwheel))
+    plot.plot(meters_per_second(rwheel))
+    plot.show()
+    
+    # plot acceleration over time
+    ldelta = numpy.diff(lwheel)
+    rdelta = numpy.diff(rwheel)
+    lvel = [(delta*1000)/15 for delta in ldelta]
+    rvel = [(delta*1000)/15 for delta in rdelta]
+    plot.plot(lvel)
+    plot.plot(rvel)
+    plot.show()
+    
+    # plot velocity over time
+    
 
 if __name__ == '__main__':
     listener()
